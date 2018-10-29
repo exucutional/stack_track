f03773ccb5528ad42a648cbb97bd6ee20c05d155
Eric Konks <43612097+exucutional@users.noreply.github.com>
2018-10-29 20:40:03 +0300

    ver.1.0

---
+++ stack/dev_room_eclipse.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ * \brief stack_unit_test.c
+ * \author Exdev
+ * \version 1.0
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdint.h>
+#include "stack_t.h"
+
+
